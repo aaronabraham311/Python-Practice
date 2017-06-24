c45955cd0aa4fa695e435c7872aa5bcbeea70e3a
akasi311 <aaronabraham311@gmail.com>
2017-06-24 15:37:27 -0600

    Uploaded hyphenate-name.py

---
+++ hyphenate-name.py
@@ -0,0 +1,7 @@
+# Getting first and last names
+firstName = raw_input("Please enter your first name: ")
+lastName = raw_input("Please enter your last name: ")
+
+print ("Your full name is: " +firstName + " " + lastName)
+print ("The first character of your name is: " + firstName[0])
+print ("Your hyphenated name is: " + firstName + " - " + lastName)
