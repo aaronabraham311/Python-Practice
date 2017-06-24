38b3a32361644d6ce5c26d776113dfcb8b687c6c
akasi311 <aaronabraham311@gmail.com>
2017-06-24 15:38:23 -0600

    Added comments and stylistic changes

--- hyphenate-name.py
+++ hyphenate-name.py
@@ -2,6 +2,7 @@
 firstName = raw_input("Please enter your first name: ")
 lastName = raw_input("Please enter your last name: ")
 
-print ("Your full name is: " +firstName + " " + lastName)
+# Printing out full name, first character, and hyphenated name
+print ("Your full name is: " + firstName + " " + lastName)
 print ("The first character of your name is: " + firstName[0])
 print ("Your hyphenated name is: " + firstName + " - " + lastName)
